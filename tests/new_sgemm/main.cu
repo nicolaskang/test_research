585f413c600c10b1b53f9bc0f1989ea21d164272
Nicolas Kang <121809625@qq.com>
2017-03-05 11:59:02 -0800

    a

--- tests/new_sgemm/main.cu
+++ tests/new_sgemm/main.cu
@@ -1,5 +1,4 @@
-/*************************************************************************** *cr *cr (C) Copyright 2010 The Board of Trustees of the
- *cr University of Illinois *cr All Rights Reserved *cr ***************************************************************************/ /* * Main entry of dense matrix-matrix multiplication kernel */ #include <stdio.h>
+#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
