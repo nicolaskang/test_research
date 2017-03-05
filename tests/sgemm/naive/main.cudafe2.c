62951ac11085283ee8c1882ce997230e9e9abbb6
Nicolas Kang <121809625@qq.com>
2017-03-05 11:52:23 -0800

    q

--- tests/sgemm/naive/main.cudafe2.c
+++
@@ -1,7 +0,0 @@
-# 1 "main.cudafe1.gpu"
-# 212 "/usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stddef.h" 3
-typedef unsigned long size_t;
-#include "crt/host_runtime.h"
-void *memcpy(void*, const void*, size_t); void *memset(void*, int, size_t);
-
-#include "main.cudafe2.stub.c"
